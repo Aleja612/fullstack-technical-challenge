0053e3d706ab3d37a0ec62f5d347d623842f44f1
Aleja612 <aleja.612@hotmail.com>
2025-11-26 20:54:14 -0500

    Add model and DTO

---
+++ product-service/src/main/java/com/code/product_service/dto/ProductResponseDTO.java
@@ -0,0 +1,18 @@
+package com.code.product_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductResponseDTO {
+private String name;
+    private String description;
+    private Double price;
+}
---
+++ product-service/src/main/java/com/code/product_service/model/Product.java
@@ -0,0 +1,29 @@
+package com.code.product_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+    
+    private String description;
+    
+    @Column(nullable = false)
+    private Double price;
+}
